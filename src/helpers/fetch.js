d60f2927732acc35d98842c87fa75f101be9986b
djesuscc <92038320+dcolc@users.noreply.github.com>
2022-02-19 20:32:11 -0600

    Add helper to fetch data

---
+++ src/helpers/fetch.js
@@ -0,0 +1,17 @@
+const baseUrl = "http://localhost:3000";
+const fetchData = (path, data, method = "GET") => {
+    const url = `${baseUrl}/${path}`;
+    if (method === "GET") {
+        return fetch(url);
+    }
+
+    return fetch(url, {
+        method,
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+};
+
+export default fetchData;
